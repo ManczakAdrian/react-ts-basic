b92f646c02a58ec1eab7184678e5e936b634fce5
Adrian Manczak <manczakadrian@gmail.com>
2025-01-17 00:56:24 +0100

    dynamic_info_about_warning

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,6 +1,8 @@
 
+import {type  ReactNode} from 'react';
 import CourseGoal from "./CourseGoal";
 import { type CourseGoal as CGoal  } from "../App";
+import InfoBox from "./InfoBox";
 
 type CourseGoalListProps={
     goals:CGoal[];
@@ -9,8 +11,32 @@ type CourseGoalListProps={
 
 };
 
-export default function CourseGoalList ({goals, onDeleteGoal}: CourseGoalListProps) {
-    return (<ul>
+export default function CourseGoalList ({
+    goals,
+    onDeleteGoal,
+}: CourseGoalListProps) {
+    if (goals.length===0){
+        return( 
+        <InfoBox mode="hint"  >
+            You have no course goals yet. Start adding some!
+                </InfoBox>
+        );
+    }
+
+    let warningBox:ReactNode;
+    if (goals. length >=4){
+        warningBox =(
+        <InfoBox mode="warning" severity="medium">
+            You're collecting a lot of goals.Don't put too much on your plate!
+        </InfoBox>
+        );
+    }
+
+    return (
+        <>
+    <ul>
+        
+        {warningBox}
         {goals.map((goal)=>(
           <li key={goal.id}>
              <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
@@ -19,5 +45,6 @@ export default function CourseGoalList ({goals, onDeleteGoal}: CourseGoalListPro
           </li>
         ))}
        </ul>
+       </>
     );
 }
